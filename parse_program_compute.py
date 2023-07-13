4a4e0e5bbe58d88ee9c96d3ca48cc5007c9503a6
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-07-13 09:47:53 +0000

    added automation to parse program compute

---
+++ parse_program_compute.py
@@ -0,0 +1,36 @@
+import re
+import configparser
+
+
+def read_compute_units(filename):
+    lines = []
+    call = ""
+    with open(filename, "r") as file:
+        for line in file:
+            call_match = re.search(r"Program log: Instruction: \w+", line)
+            match = re.search(
+                r"Program \w+ consumed (\d+) of 200000 compute units", line
+            )
+            if call_match:
+                call = line.split("Instruction:")[1].strip()
+            if match:
+                lines.append(
+                    {call: int(line.split("consumed")[1].split("of")[0].strip())}
+                )
+    return lines
+
+
+def read_configuration(filename):
+    config = configparser.ConfigParser()
+    config.read(filename)
+    return dict(config.items("programs.localnet"))
+
+
+configuration = read_configuration("Anchor.toml")
+for program in configuration:
+    program_key = configuration[program].strip('"')
+    program_log_directory = ".anchor/program-logs"
+    program_log_file = f"{program_key}.{program}.log"
+    result = read_compute_units(f"{program_log_directory}/{program_log_file}")
+    for line in result:
+        print(line)
